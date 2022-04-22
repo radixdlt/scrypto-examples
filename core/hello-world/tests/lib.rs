bb5f56fd235bfac78c2e4aba086ecc17959dd921
0xOmarA <OmarAbdulla7@hotmail.com>
2022-04-22 18:54:17 +0300

    Updated the hello world example

--- core/hello-world/tests/lib.rs
+++ core/hello-world/tests/lib.rs
@@ -7,29 +7,31 @@ fn test_hello() {
     // Set up environment.
     let mut ledger = InMemorySubstateStore::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, false);
-    let key = executor.new_public_key();
-    let account = executor.new_account(key);
-    let package = executor
-        .publish_package(include_code!("hello_world"))
-        .unwrap();
+    let (pk, sk, account) = executor.new_account();
+    let package = executor.publish_package(compile_package!()).unwrap();
 
-    // Test the `instantiate_hello` function.
-    let transaction1 = TransactionBuilder::new(&executor)
-        .call_function(package, "Hello", "instantiate_hello", vec![], None)
-        .build(vec![])
-        .unwrap();
-    let receipt1 = executor.run(transaction1).unwrap();
+    // Test the `instantiate_hello_nft` function.
+    let transaction1 = TransactionBuilder::new()
+        .call_function(package, "Hello", "instantiate_hello", vec![scrypto_encode(&Decimal(5))])
+        .call_method_with_all_resources(account, "deposit_batch")
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt1 = executor.validate_and_execute(&transaction1).unwrap();
     println!("{:?}\n", receipt1);
     assert!(receipt1.result.is_ok());
 
-    // Test the `free_token` method.
-    let component = receipt1.component(0).unwrap();
-    let transaction2 = TransactionBuilder::new(&executor)
-        .call_method(component, "free_token", vec![], Some(account))
+    // Test the `buy_ticket_by_id` method.
+    let component = receipt1.new_component_addresses[0];
+    let transaction2 = TransactionBuilder::new()
+        .call_method(
+            component,
+            "free_token",
+            vec![],
+        )
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![])
-        .unwrap();
-    let receipt2 = executor.run(transaction2).unwrap();
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt2 = executor.validate_and_execute(&transaction2).unwrap();
     println!("{:?}\n", receipt2);
     assert!(receipt2.result.is_ok());
 }
