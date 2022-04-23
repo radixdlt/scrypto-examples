f6e0c677bbfc0662d96886faa0c082d64f8dfbc3
0xOmarA <OmarAbdulla7@hotmail.com>
2022-04-24 01:31:16 +0300

    Updated defi fully

--- defi/demo.sh
+++ defi/demo.sh
@@ -5,60 +5,65 @@ set -e
 
 cd "$(dirname "$0")"
 
-cargo install --path ../../simulator
+# cargo install --path ../../simulator
 
 # reset database
 resim reset
 
 # new account
-out=`resim new-account | tee /dev/tty | awk '/Account address:|Public key:/ {print $NF}'`
+out=`resim new-account | tee /dev/tty | awk '/Account component address:|Public key:|Private key:/ {print $NF}'`
 acc1_address=`echo $out | cut -d " " -f1`
 acc1_pub_key=`echo $out | cut -d " " -f2`
-out=`resim new-account | tee /dev/tty | awk '/Account address:|Public key:/ {print $NF}'`
+acc1_priv_key=`echo $out | cut -d " " -f3`
+out=`resim new-account | tee /dev/tty | awk '/Account component address:|Public key:|Private key:/ {print $NF}'`
 acc2_address=`echo $out | cut -d " " -f1`
 acc2_pub_key=`echo $out | cut -d " " -f2`
-out=`resim new-account | tee /dev/tty | awk '/Account address:|Public key:/ {print $NF}'`
+acc2_priv_key=`echo $out | cut -d " " -f3`
+out=`resim new-account | tee /dev/tty | awk '/Account component address:|Public key:|Private key:/ {print $NF}'`
 acc3_address=`echo $out | cut -d " " -f1`
 acc3_pub_key=`echo $out | cut -d " " -f2`
-out=`resim new-account | tee /dev/tty | awk '/Account address:|Public key:/ {print $NF}'`
+acc3_priv_key=`echo $out | cut -d " " -f3`
+out=`resim new-account | tee /dev/tty | awk '/Account component address:|Public key:|Private key:/ {print $NF}'`
 acc4_address=`echo $out | cut -d " " -f1`
 acc4_pub_key=`echo $out | cut -d " " -f2`
+acc4_priv_key=`echo $out | cut -d " " -f3`
 
 # generate acc1_mint_badge token
-acc1_mint_badge=`resim new-badge-fixed --name MintBadge 1 | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+acc1_mint_badge=`resim new-badge-fixed --name MintBadge 1 | tee /dev/tty | awk '/Resource:/ {print $NF}'`
 
 # mint btc
-btc=`resim new-token-mutable $acc1_mint_badge --name Bitcoin --symbol BTC --description "Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+btc=`resim new-token-mutable $acc1_mint_badge --name Bitcoin --symbol BTC --description "Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries." | tee /dev/tty | awk '/Resource:/ {print $NF}'`
 resim mint 18843462 $btc $acc1_mint_badge
 
 # mint ethereum
-eth=`resim new-token-mutable $acc1_mint_badge --name Ethereum --symbol ETH --description "Ethereum is a decentralized, open-source blockchain with smart contract functionality." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+eth=`resim new-token-mutable $acc1_mint_badge --name Ethereum --symbol ETH --description "Ethereum is a decentralized, open-source blockchain with smart contract functionality." | tee /dev/tty | awk '/Resource:/ {print $NF}'`
 resim mint 117921786 $eth $acc1_mint_badge
 
 # mint USD
-usd=`resim new-token-mutable $acc1_mint_badge --name "US Dollar" --symbol USD --description "The United States dollar is the official currency of the United States and its territories." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 19677000000000 $usd $acc1_mint_badge
+usd=`resim new-token-mutable $acc1_mint_badge --name "US Dollar" --symbol USD --description "The United States dollar is the official currency of the United States and its territories." | tee /dev/tty | awk '/Resource:/ {print $NF}'`
+resim mint 100000000000 $usd $acc1_mint_badge
 
 # mint GBP
-gbp=`resim new-token-mutable $acc1_mint_badge --name "Pound sterling" --symbol GBP --description "The pound sterling, known in some contexts simply as the pound or sterling, is the official currency of the United Kingdom, Jersey, Guernsey, the Isle of Man, Gibraltar, South Georgia and the South Sandwich Islands, the British Antarctic Territory, and Tristan da Cunha." | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 2896859000000 $gbp $acc1_mint_badge
+gbp=`resim new-token-mutable $acc1_mint_badge --name "Pound sterling" --symbol GBP --description "The pound sterling, known in some contexts simply as the pound or sterling, is the official currency of the United Kingdom, Jersey, Guernsey, the Isle of Man, Gibraltar, South Georgia and the South Sandwich Islands, the British Antarctic Territory, and Tristan da Cunha." | tee /dev/tty | awk '/Resource:/ {print $NF}'`
+resim mint 100000000000 $gbp $acc1_mint_badge
 
 # mint XRD
-xrd=`resim new-token-mutable $acc1_mint_badge --name "Radix Token" --symbol XRD | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim mint 24000000000 $xrd $acc1_mint_badge
+xrd=`resim new-token-mutable $acc1_mint_badge --name "Radix Token" --symbol XRD | tee /dev/tty | awk '/Resource:/ {print $NF}'`
+resim mint 100000000000 $xrd $acc1_mint_badge
 
 # mint SNX
-snx=`resim new-token-mutable $acc1_mint_badge --name "Synthetics Token" --symbol SNX --description "A token which is used in the synthetics component for collateral" | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
+snx=`resim new-token-mutable $acc1_mint_badge --name "Synthetics Token" --symbol SNX --description "A token which is used in the synthetics component for collateral" | tee /dev/tty | awk '/Resource:/ {print $NF}'`
 resim mint 114841533.01 $snx $acc1_mint_badge
 
 # publish PriceOracle
 price_oracle_package=`resim publish ./price-oracle | tee /dev/tty | awk '/Package:/ {print $NF}'`
-out=`resim call-function $price_oracle_package PriceOracle instantiate_oracle 1 | tee /dev/tty | awk '/Component:|ResourceDef:/ {print $NF}'`
-price_oracle_update_auth=`echo $out | cut -d " " -f1`
-price_oracle_component=`echo $out | cut -d " " -f2`
+out=`resim call-function $price_oracle_package PriceOracle instantiate_oracle 1 | tee /dev/tty | awk '/Component:|Resource:/ {print $NF}'`
+price_oracle_component=`echo $out | cut -d " " -f1`
+price_oracle_update_auth=`echo $out | cut -d " " -f2`
 
 # publish Radiswap
-radiswap_package=`resim publish ./radiswap | tee /dev/tty | awk '/Package:/ {print $NF}'`
+radiswap_package="0107467fe140289bec61d5ec6be68d6c7adc88ea5528d90c28b9f5"
+resim publish ./radiswap --package-address $radiswap_package
 
 # publish AutoLend
 auto_lend_package=`resim publish ./auto-lend | tee /dev/tty | awk '/Package:/ {print $NF}'`
@@ -76,14 +81,17 @@ perpetual_futures_component=`resim call-function $perpetual_futures_package Clea
 xrd_snx_radiswap_component=`resim call-function $radiswap_package Radiswap instantiate_pool 1000000,$xrd 38271,$snx 1000000 LPT LPToken https://www.example.com/ 0.001 | tee /dev/tty | awk '/Component:/ {print $NF}'`
 
 # Update price
-resim call-method $price_oracle_component update_price $btc $usd 57523  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $eth $usd 3763  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $btc $gbp 41950  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $eth $gbp 2746  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $btc $eth 15  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $xrd $usd 0.4  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $snx $usd 10.40  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $btc $usd 66050.98  1,$price_oracle_update_auth
+echo "CALL_METHOD ComponentAddress(\"$acc1_address\") \"create_proof\" ResourceAddress(\"$price_oracle_update_auth\");" > tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$btc\") ResourceAddress(\"$usd\") Decimal(\"57523\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$eth\") ResourceAddress(\"$usd\") Decimal(\"3763\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$btc\") ResourceAddress(\"$gbp\") Decimal(\"41950\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$eth\") ResourceAddress(\"$gbp\") Decimal(\"2746\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$btc\") ResourceAddress(\"$eth\") Decimal(\"15\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$xrd\") ResourceAddress(\"$usd\") Decimal(\"0.4\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$snx\") ResourceAddress(\"$usd\") Decimal(\"10.40\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$btc\") ResourceAddress(\"$usd\") Decimal(\"66050.98\");" >> tx.rtm
+resim run tx.rtm
+rm tx.rtm
 resim call-method $price_oracle_component get_price $btc $eth
 resim call-method $price_oracle_component get_price $eth $btc
 
@@ -125,16 +133,19 @@ set -x
 #====================
 
 # Create TESLA resource with no supply
-tesla=`resim new-token-fixed --name "Tesla Token" --symbol "TESLA" 0 | tee /dev/tty | awk '/ResourceDef:/ {print $NF}'`
-resim call-method $price_oracle_component update_price $tesla $usd 1162.00  1,$price_oracle_update_auth
-resim call-method $price_oracle_component update_price $xrd $snx 0.03901819  1,$price_oracle_update_auth
+tesla=`resim new-token-fixed --name "Tesla Token" --symbol "TESLA" 0 | tee /dev/tty | awk '/Resource:/ {print $NF}'`
+echo "CALL_METHOD ComponentAddress(\"$acc1_address\") \"create_proof\" ResourceAddress(\"$price_oracle_update_auth\");" > tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$tesla\") ResourceAddress(\"$usd\") Decimal(\"1162.00\");" >> tx.rtm
+echo "CALL_METHOD ComponentAddress(\"$price_oracle_component\") \"update_price\" ResourceAddress(\"$xrd\") ResourceAddress(\"$snx\") Decimal(\"0.03901819\");" >> tx.rtm
+resim run tx.rtm
+rm tx.rtm
 
 # Publish mutual farm package
 mutual_farm_package=`resim publish mutual-farm | tee /dev/tty | awk '/Package:/ {print $NF}'`
 
 # Instantiate mutual farm
 out=`resim call-function $mutual_farm_package MutualFarm instantiate_farm $price_oracle_component $xrd_snx_radiswap_component $synthetics_component "TESLA" $tesla 1000 1000000,$xrd $snx $usd | tee /dev/tty | awk '/Component:/ {print $NF}'`
-mutual_farm_component=`echo $out | cut -d " " -f2`
+mutual_farm_component=`echo $out | cut -d " " -f1`
 resim show $mutual_farm_component
 resim show $acc1_address
 
