c5b6e8c3a4bd0b62f8a96ba37321ad7a278cc275
0xOmarA <OmarAbdulla7@hotmail.com>
2022-04-24 05:25:33 +0300

    Updated core/flat-admin tests

--- core/flat-admin/tests/lib.rs
+++ core/flat-admin/tests/lib.rs
@@ -7,36 +7,33 @@ fn test_create_additional_admin() {
     // Set up environment.
     let mut ledger = InMemorySubstateStore::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, false);
-    let key = executor.new_public_key();
-    let account = executor.new_account(key);
-    let package = executor
-        .publish_package(include_code!("flat_admin"))
-        .unwrap();
+    let (pk, sk, account) = executor.new_account();
+    let package = executor.publish_package(compile_package!()).unwrap();
 
     // Test the `instantiate_flat_admin` function.
-    let transaction1 = TransactionBuilder::new(&executor)
-        .call_function(package, "FlatAdmin", "instantiate_flat_admin", vec!["test".to_string()], None)
+    let transaction1 = TransactionBuilder::new()
+        .call_function(package, "FlatAdmin", "instantiate_flat_admin", args!["test"])
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt1 = executor.run(transaction1).unwrap();
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt1 = executor.validate_and_execute(&transaction1).unwrap();
     println!("{:?}\n", receipt1);
     assert!(receipt1.result.is_ok());
 
     // Test the `create_additional_admin` method.
-    let flat_admin = receipt1.component(0).unwrap();
-    let admin_badge = receipt1.resource_def(1).unwrap();
-    let transaction2 = TransactionBuilder::new(&executor)
+    let flat_admin = receipt1.new_component_addresses[0];
+    let admin_badge = receipt1.new_resource_addresses[1];
+    let transaction2 = TransactionBuilder::new()
+        .create_proof_from_account_by_amount(dec!("1"), admin_badge, account)
         .call_method(
             flat_admin,
             "create_additional_admin",
-            vec![format!("1,{}", admin_badge)],
-            Some(account),
+            args![],
         )
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt2 = executor.run(transaction2).unwrap();
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt2 = executor.validate_and_execute(&transaction2).unwrap();
     println!("{:?}\n", receipt2);
     assert!(receipt2.result.is_ok());
 }
