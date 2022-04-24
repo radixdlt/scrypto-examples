79f579a216b8e61695a5fd16e3d66f0da00f27c7
0xOmarA <OmarAbdulla7@hotmail.com>
2022-04-24 05:20:31 +0300

    Updated core/cross-blueprint-call tests

--- core/cross-blueprint-call/tests/lib.rs
+++ core/cross-blueprint-call/tests/lib.rs
@@ -7,35 +7,32 @@ fn test_proxy_1() {
     // Set up environment.
     let mut ledger = InMemorySubstateStore::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, false);
-    let key = executor.new_public_key();
-    let account = executor.new_account(key);
-    let package = executor
-        .publish_package(include_code!("cross_blueprint_call"))
-        .unwrap();
+    let (pk, sk, account) = executor.new_account();
+    let package = executor.publish_package(compile_package!()).unwrap();
 
     // Airdrop blueprint.
     executor.overwrite_package(
-        Address::from_str("01bda8686d6c2fa45dce04fac71a09b54efbc8028c23aac74bc00e").unwrap(),
-        include_code!("cross_blueprint_call"),
+        PackageAddress::from_str("01e9917332573b6332ffaaadc96bc1509cc24ef8aa69d1cd117d39").unwrap(),
+        compile_package!()
     );
 
     // Test the `instantiate_proxy` function.
-    let transaction1 = TransactionBuilder::new(&executor)
-        .call_function(package, "Proxy1", "instantiate_proxy", vec![], None)
-        .build(vec![])
-        .unwrap();
-    let receipt1 = executor.run(transaction1).unwrap();
+    let transaction1 = TransactionBuilder::new()
+        .call_function(package, "Proxy1", "instantiate_proxy", vec![])
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt1 = executor.validate_and_execute(&transaction1).unwrap();
     println!("{:?}\n", receipt1);
     assert!(receipt1.result.is_ok());
 
     // Test the `get_gumball` method.
-    let component = receipt1.component(0).unwrap();
-    let transaction2 = TransactionBuilder::new(&executor)
-        .call_method(component, "free_token", vec![], Some(account))
+    let component = receipt1.new_component_addresses[0];
+    let transaction2 = TransactionBuilder::new()
+        .call_method(component, "free_token", vec![])
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt2 = executor.run(transaction2).unwrap();
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt2 = executor.validate_and_execute(&transaction2).unwrap();
     println!("{:?}\n", receipt2);
     assert!(receipt2.result.is_ok());
 }
@@ -45,35 +42,32 @@ fn test_proxy_2() {
     // Set up environment.
     let mut ledger = InMemorySubstateStore::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, false);
-    let key = executor.new_public_key();
-    let account = executor.new_account(key);
-    let package = executor
-        .publish_package(include_code!("cross_blueprint_call"))
-        .unwrap();
+    let (pk, sk, account) = executor.new_account();
+    let package = executor.publish_package(compile_package!()).unwrap();
 
     // Airdrop blueprint.
     executor.overwrite_package(
-        Address::from_str("01bda8686d6c2fa45dce04fac71a09b54efbc8028c23aac74bc00e").unwrap(),
-        include_code!("cross_blueprint_call"),
+        PackageAddress::from_str("01e9917332573b6332ffaaadc96bc1509cc24ef8aa69d1cd117d39").unwrap(),
+        compile_package!()
     );
 
     // Test the `instantiate_proxy` function.
-    let transaction1 = TransactionBuilder::new(&executor)
-        .call_function(package, "Proxy2", "instantiate_proxy", vec![], None)
-        .build(vec![])
-        .unwrap();
-    let receipt1 = executor.run(transaction1).unwrap();
+    let transaction1 = TransactionBuilder::new()
+        .call_function(package, "Proxy2", "instantiate_proxy", vec![])
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt1 = executor.validate_and_execute(&transaction1).unwrap();
     println!("{:?}\n", receipt1);
     assert!(receipt1.result.is_ok());
 
     // Test the `get_gumball` method.
-    let component = receipt1.component(0).unwrap();
-    let transaction2 = TransactionBuilder::new(&executor)
-        .call_method(component, "free_token", vec![], Some(account))
+    let component = receipt1.new_component_addresses[0];
+    let transaction2 = TransactionBuilder::new()
+        .call_method(component, "free_token", vec![])
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt2 = executor.run(transaction2).unwrap();
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt2 = executor.validate_and_execute(&transaction2).unwrap();
     println!("{:?}\n", receipt2);
     assert!(receipt2.result.is_ok());
 }
