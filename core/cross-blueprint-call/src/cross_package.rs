cb900122849689652b67ae2b6e23bd1a7b9360f7
0xOmarA <OmarAbdulla7@hotmail.com>
2022-08-24 14:39:00 +0300

    Updated the cross blueprint call example

--- core/cross-blueprint-call/src/cross_package.rs
+++ core/cross-blueprint-call/src/cross_package.rs
@@ -3,32 +3,66 @@ use scrypto::prelude::*;
 import! {
 r#"
 {
-    "package_address": "01e9917332573b6332ffaaadc96bc1509cc24ef8aa69d1cd117d39",
-    "blueprint_name": "Airdrop",
-    "functions": [
-      {
-        "name": "instantiate_airdrop",
-        "inputs": [],
-        "output": {
-          "type": "Custom",
-          "name": "ComponentAddress",
-          "generics": []
-        }
+  "package_address": "package_sim1qx7304nj09gmp8t8kd2xv6t9yxg7vug8su6wq9kjfutqzl29ru",
+  "blueprint_name": "Airdrop",
+  "abi": {
+    "structure": {
+      "type": "Struct",
+      "name": "Airdrop",
+      "fields": {
+        "type": "Named",
+        "named": [
+          [
+            "tokens",
+            {
+              "type": "Custom",
+              "type_id": 179,
+              "generics": []
+            }
+          ]
+        ]
       }
-    ],
-    "methods": [
+    },
+    "fns": [
       {
-        "name": "free_token",
-        "mutability": "Mutable",
-        "inputs": [],
+        "ident": "instantiate_airdrop",
+        "mutability": null,
+        "input": {
+          "type": "Struct",
+          "name": "Airdrop_instantiate_airdrop_Input",
+          "fields": {
+            "type": "Named",
+            "named": []
+          }
+        },
         "output": {
           "type": "Custom",
-          "name": "Bucket",
+          "type_id": 129,
           "generics": []
-        }
+        },
+        "export_name": "Airdrop_instantiate_airdrop"
+      },
+      {
+        "ident": "free_token",
+        "mutability": "Mutable",
+        "input": {
+          "type": "Struct",
+          "name": "Airdrop_free_token_Input",
+          "fields": {
+            "type": "Named",
+            "named": []
+          }
+        },
+        "output": {
+          "type": "Custom",
+          "type_id": 177,
+          "generics": []
+        },
+        "export_name": "Airdrop_free_token"
       }
     ]
   }
+}
 "#
 }
 
@@ -40,9 +74,9 @@ blueprint! {
     impl Proxy1 {
         pub fn instantiate_proxy() -> ComponentAddress {
             Self {
-                // The instantiate_airdrop() function returns a generic ComponentAddress which we store to make calls 
+                // The instantiate_airdrop() function returns a generic ComponentAddress which we store to make calls
                 // to the component at a later point.
-                airdrop: Airdrop::instantiate_airdrop().into(),
+                airdrop: Airdrop::instantiate_airdrop(),
             }
             .instantiate()
             .globalize()
--- core/cross-blueprint-call/src/intra_package.rs
+++ core/cross-blueprint-call/src/intra_package.rs
@@ -1,17 +1,21 @@
+use crate::airdrop::*;
 use scrypto::prelude::*;
-
-use crate::airdrop::Airdrop;
 
 blueprint! {
     struct Proxy2 {
-        airdrop: Airdrop,
+        airdrop: Airdrop_Component,
     }
 
     impl Proxy2 {
         pub fn instantiate_proxy() -> ComponentAddress {
             Self {
                 // The instantiate_airdrop() function returns a generic Component. We use `.into()` to convert it into an `Airdrop`.
-                airdrop: Airdrop::instantiate_airdrop().into(),
+                airdrop: Airdrop_Component {
+                    component: scrypto::component::Component::try_from(
+                        Airdrop_Component::instantiate_airdrop().to_vec().as_slice(),
+                    )
+                    .unwrap(),
+                },
             }
             .instantiate()
             .globalize()
