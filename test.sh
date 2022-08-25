3bee6cb94650793d21c7c019fdd5736899090e6b
Yulong Wu <yulong.wu@rdx.works>
2022-08-25 12:18:44 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -5,12 +5,8 @@ set -e
 
 cd "$(dirname "$0")"
 
-(cd core; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo build --release --target wasm32-unknown-unknown --manifest-path {}/Cargo.toml)
-(cd defi; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo build --release --target wasm32-unknown-unknown --manifest-path {}/Cargo.toml)
-(cd nft; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo build --release --target wasm32-unknown-unknown --manifest-path {}/Cargo.toml)
-
-(cd core; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo test --manifest-path {}/Cargo.toml)
-(cd defi; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo test --manifest-path {}/Cargo.toml)
-(cd nft; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' cargo test --manifest-path {}/Cargo.toml)
+(cd core; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' scrypto test --path {})
+(cd defi; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' scrypto test --path {})
+(cd nft; find . -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -n1 -I '{}' scrypto test --path {})
 
 echo "Congrats! All tests passed."
