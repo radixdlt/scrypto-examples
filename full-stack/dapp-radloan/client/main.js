cc045d93f5618709d9688e2114ae5834dc02a387
Bryan <motosharpley@gmail.com>
2023-04-18 14:00:13 -0700

    initial front end commit

--- full-stack/dapp-radloan/client/main.js
+++ full-stack/dapp-radloan/client/main.js
@@ -1,48 +1,45 @@
-import './style.css'
-import scryptoLogo from './scryptoLogo.png'
-import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit'
+import { RadixDappToolkit, ManifestBuilder, Decimal, Bucket, Expression, ResourceAddress } from '@radixdlt/radix-dapp-toolkit'
 
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://docs-babylon.radixdlt.com/main/scrypto/introduction.html" target="_blank">
-      <img src="${scryptoLogo}" class="logo vanilla" alt="Scrypto logo" />
-    </a>
-    <h1>Hello Scrypto!</h1>  
-    <p class="read-the-docs">
-      Click on the Scrypto logo to learn more
-    </p>
-    <div class="card">
-      <radix-connect-button />
-    </ div>  
-  </div>
-`
 const rdt = RadixDappToolkit(
   {
     dAppDefinitionAddress:
-      "account_tdx_22_1pz7vywgwz4fq6e4v3aeeu8huamq0ctmsmzltay07vzpqm82mp5",
-    dAppName: "Name of your dApp",
+      'account_tdx_22_1pz7vywgwz4fq6e4v3aeeu8huamq0ctmsmzltay07vzpqm82mp5',
+    dAppName: 'RadLoan',
   },
   (requestData) => {
     requestData({
-      accounts: { quantifier: "atLeast", quantity: 1 },
+      accounts: { quantifier: 'atLeast', quantity: 1 },
     }).map(({ data: { accounts } }) => {
       // set your application state
-      console.log("requestData accounts: ",accounts)
-    });
+      console.log("requestData accounts: ", accounts)
+    })
   },
   {
-    networkId: 11, // for betanet 01 for mainnet
+    networkId: 11,
     onDisconnect: () => {
       // clear your application state
     },
     onInit: ({ accounts }) => {
       // set your initial application state
-      console.log("onInit accounts: ",accounts)
+      console.log("onInit accounts: ", accounts)
     },
   }
-);
+)
 
+
+// Handle Form Data
+const form = document.getElementById('transactionForm')
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  console.log(Object.fromEntries(data));
+
+})
+
+
+// ####### RadLoan Methods #######
+// instantiate_default(initial_liquidity: Bucket)
+// available_liquidity(&self)
+// add_liquidity(&mut self, tokens: Bucket)
+// take_loan(&mut self, loan_amount: Decimal)
+// repay_loan(&mut self, loan_repayment: Bucket, loan_terms: Bucket)
