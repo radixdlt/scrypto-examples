42fcad3e0b654b898ea7ab2715677b3948e5d60a
jake-rdxworks <jakephucmai@gmail.com>
2023-07-18 16:36:44 -0400

    Update flat admin

--- core/flat-admin/src/lib.rs
+++ core/flat-admin/src/lib.rs
@@ -13,17 +13,18 @@ mod flat_admin {
         }
     }
     struct FlatAdmin {
-        first_admin_badge: ResourceManager,
+        admin_badge: ResourceManager,
     }
 
     impl FlatAdmin {
         pub fn instantiate_flat_admin(badge_name: String) -> (Global<FlatAdmin>, Bucket) {
 
-            let blueprint_id = Runtime::blueprint_id();
-            let (address_reservation, component_address) = Runtime::allocate_component_address(blueprint_id);
+            // Creating a GlobalAddressReservation and ComponentAddress to use for the actor virtual badge pattern.
+            let (address_reservation, component_address) = 
+                Runtime::allocate_component_address(Runtime::blueprint_id());
 
             // Create the ResourceManager for a mutable supply admin badge
-            let first_admin_badge = ResourceBuilder::new_fungible(OwnerRole::None)
+            let admin_badge = ResourceBuilder::new_fungible(OwnerRole::None)
                 .divisibility(DIVISIBILITY_NONE)
                 .metadata(metadata!(
                         init {
@@ -43,38 +44,39 @@ mod flat_admin {
 
             // Initialize our component, placing the minting authority badge within its vault, where it will remain forever
             let component = Self {
-                first_admin_badge: first_admin_badge.resource_manager(),
+                admin_badge: admin_badge.resource_manager(),
             }
             .instantiate()
-            .prepare_to_globalize(OwnerRole::Updatable(rule!(require(first_admin_badge.resource_address()))))
+            .prepare_to_globalize(OwnerRole::Updatable(rule!(require(admin_badge.resource_address()))))
             .roles(
                 roles!(
-                    admin => rule!(require(first_admin_badge.resource_address()));
+                    admin => rule!(require(admin_badge.resource_address()));
                 )
             )
             .with_address(address_reservation)
             .globalize();
 
             // Return the instantiated component and the admin badge we just minted
-            (component, first_admin_badge)
+            (component, admin_badge)
         }
 
         // Any existing admin may create another admin token
         pub fn create_additional_admin(&mut self) -> Bucket {
             // The "authorize" method provides a convenient shortcut to make use of the mint authority badge within our vault without removing it
-            return self.first_admin_badge.mint(dec!(1));
+            return self.admin_badge.mint(dec!(1));
+
         }
 
         pub fn destroy_admin_badge(&mut self, to_destroy: Bucket) {
             assert!(
-                to_destroy.resource_address() == self.first_admin_badge.address(),
+                to_destroy.resource_address() == self.admin_badge.address(),
                 "Can not destroy the contents of this bucket!"
             );
             to_destroy.burn();
         }
 
         pub fn get_admin_badge_address(&self) -> ResourceAddress {
-            return self.first_admin_badge.address();
+            return self.admin_badge.address();
         }
     }
 }
